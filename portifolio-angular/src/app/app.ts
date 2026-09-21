ac6c47c7413b69c298272088466dd9c7468e9162
Matheus Felix Mendes <20241ctb0100037@estudantes.ifpr.edu.br>
2026-09-21 21:00:51 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

--- portifolio-angular/src/app/app.ts
+++ portifolio-angular/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
-export class App {
-  protected readonly title = signal('Meu Portifólio');
-}
+export class App {}
---
+++ portifolio-angular/src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCard, MatCardModule } from '@angular/material/card';
+
+@Component({
+  imports: [],
+  selector: 'app-home',
+  imports: [MatCardModule]
+  styleUrl: './home.css',
+  templateUrl: './home.html',
+})
+export class Home {}
